0530acde2bed267cbaf23a35f95428515e1d27f1
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-07-26 18:50:05 +0300

    Move your interfaces to the right directory

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,19 +1,21 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { LayoutProps } from "@/interface";
+
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
+    return (
     <>
-      <Header />
-      <main className="pt-28 min-h-screen">{children}</main>
-      <Footer />
+        <Header />
+        <main className="pt-28 min-h-screen">{children}</main>
+        <Footer />
     </>
-  );
+    );
 };
 
 export default Layout;
